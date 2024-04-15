ec5adf6d38d8144ad35912d491e39f6b3a3582b2
wallace13 <wsalesg@hotmail.com>
2024-04-15 13:53:31 -0300

    Python Exercicios

---
+++ python/Exercicio01.py
@@ -0,0 +1,9 @@
+def main():
+    nome = raw_input("Informe seu nome completo: ")
+    sexo = raw_input("Informe seu sexo: ")
+    
+    print("Nome: ",nome)
+    print("Sexo: ",sexo)
+
+if __name__ == "__main__":
+ main()
