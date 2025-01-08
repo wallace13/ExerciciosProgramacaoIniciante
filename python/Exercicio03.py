ab86cca78be4c51537b90ff3a09f69d38ab87a63
wallace13 <wsalesg@hotmail.com>
2025-01-07 21:25:21 -0300

    exercicio 03

---
+++ python/Exercicio03.py
@@ -0,0 +1,6 @@
+dolar = float(input("Digite o valor em dolar: "))
+
+#1 Dólar dos Estados Unidos/USD (220) = 4,8931 Real/BRL (790)
+conversao = (dolar * 4.8931)
+
+print("Valor em real: R$", conversao)
