78b916b259a81a391a1dc2be0586b617759da4a7
wallace13 <wsalesg@hotmail.com>
2025-01-07 21:18:38 -0300

    exercicio02

---
+++ python/Exercicio02.py
@@ -0,0 +1,9 @@
+n1 = float(input("Informe um número1: "))
+n2 = float(input("Informe um número2: "))
+n3 = float(input("Informe um número3: "))
+n4 = float(input("Informe um número4: "))
+
+soma = (n1+n2+n3+n4)
+multiplicacao = ((n1*n2)*(n3*n4))
+print("a) Resultado de suas adições: ",soma)
+print("b) Resultado de suas multiplicações: ", multiplicacao)
