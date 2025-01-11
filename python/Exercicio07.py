a968148917aebe7fe7dcf25016bc67da21909007
wallace13 <wsalesg@hotmail.com>
2025-01-11 18:17:17 -0300

    exercicio 07

---
+++ python/Exercicio07.py
@@ -0,0 +1,7 @@
+n1 = float(input("Informe um número1: "))
+n2 = float(input("Informe um número2: "))
+n3 = float(input("Informe um número3: "))
+
+media = (n1+n2+n3)/3
+
+print("Média Aritmética: ",media)
