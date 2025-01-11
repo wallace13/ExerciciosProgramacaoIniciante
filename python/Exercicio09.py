8469e8568baf9e1c3fce87c36902a500beff0fa8
wallace13 <wsalesg@hotmail.com>
2025-01-11 18:25:20 -0300

    exercicio 09

---
+++ python/Exercicio09.py
@@ -0,0 +1,6 @@
+import math
+number = int(input("Digite um número Inteiro: "))
+
+print("a) O próprio número:             ",int(number),"\n")
+print("b) O quadrado deste número:      ",(int(number)*2),"\n")
+print("c) A raiz quadrada deste número: ",math.sqrt(int(number)),"\n")
