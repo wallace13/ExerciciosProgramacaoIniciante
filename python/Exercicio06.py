d8bd60ebac58fb34e32487b3829e1efc28bede2a
wallace13 <wsalesg@hotmail.com>
2025-01-11 18:13:44 -0300

    exercicio 06

---
+++ python/Exercicio06.py
@@ -0,0 +1,6 @@
+fahrenhei = float(input("Informe a temperatura em graus Fahrenhei: "))
+
+celsius = (fahrenhei - 32)* 5/9
+
+print(f"Temperatura em Fahrenhei: {fahrenhei}")
+print(f"Temperatura em Celsius:   {celsius}")
