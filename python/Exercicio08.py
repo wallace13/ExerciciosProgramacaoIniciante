83e5c83acdb1659a99b90aa032a63c01c47722b7
wallace13 <wsalesg@hotmail.com>
2025-01-11 18:21:48 -0300

    exercicio 08

---
+++ python/Exercicio08.py
@@ -0,0 +1,4 @@
+distancia = float(input("Digite a Distancia a ser Percorrida: "))
+consumo = float(input("Digite o Consumo Médio do Automóvel: "))
+
+print("Litros Necessarios: ",(distancia/consumo),"L")
